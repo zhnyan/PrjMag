363ce6aae2170401179b17b126d3259f1000c591
zhnyan <zhn.yan@qq.com>
2018-04-29 08:43:39 +0800

    add command argv. can assign any package list to install like this: python install.py packagelist

--- install.py
+++ install.py
@@ -1,4 +1,14 @@
 import os
+import sys
 
-os.system('pip install -r LibList --timeout 300')
-os.system('jupyter contrib nbextension install --user')
+package_list = sys.argv[1]
+def install(package_list):
+    # update pip
+    os.system('python -m pip install --upgrade pip')
+    # install packages
+    os.system('pip install -r ' + package_list + ' --timeout 300')
+    # configure jupyter
+    os.system('jupyter contrib nbextension install --user')
+
+if __name__ == '__main__':
+    install( package_list )
